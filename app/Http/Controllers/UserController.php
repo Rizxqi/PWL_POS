1fa58d175c13ad950f162440e3de5c71f568fcfe
Rizxqi <rizqidewanto23@gmail.com>
2025-03-13 11:11:50 +0700

    Practicum 2.6 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,32 +10,70 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ],
-        );
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
 
-        $user->username = 'manager12';
-        
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username','level_id']); //true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama','username'])); // true
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
 
-        // $user->isClean(); // false
-        // $user->isClean('username'); // false
-        // $user->isClean('nama'); // true
-        // $user->isClean(['nama','username']); //false
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
 
-        // $user->save();
+    public function ubah($id)
+    {
+        // $user = UserModel::find($id);
 
-        // $user->isDirty(); // false
-        // $user->isClean(); // true
-        // dd($user->isDirty());
+        // // if (!$user) {
+        // //     return redirect('/user')->with('error', 'User tidak ditemukan');
+        // // }
+        // // $user = User::find($id);
+        // return view('user_ubah', ['data' => $user]);
+
+        // Fetch the user by ID
+        $user = UserModel::find($id);
+
+        // Check if the user exists
+        if (!$user) {
+            return redirect()->route('/user')->with('error', 'User not found');
+        }
+
+        // Pass the user data to the view
+        return view('user_ubah', ['data' => $user]);
+
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+    
+        // if (!$user) {
+        //     return redirect('/user')->with('error', 'User tidak ditemukan');
+        // }
+    
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->level_id = $request->level_id;
+    
+        $user->save();
+        return redirect('/user')->with('success', 'User berhasil diperbarui');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 }
