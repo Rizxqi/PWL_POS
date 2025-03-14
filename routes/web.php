4f76a3e9201d810c7b696cd803e7d77c3d33c6d4
Rizxqi <rizqidewanto23@gmail.com>
2025-03-14 21:30:58 +0700

    Practicum 2 Jobsheet 5 (Layouting)

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 
 
-Route::get('/', function(){
-    return view('welcome');
-});
-
-Route::get('/level',[LevelController::class,'index']);
-Route::get('/kategori',[KategoriController::class,'index']);
-Route::get('/user',[UserController::class,'index'])->name('/user');
-
-Route::get('/user/tambah',[UserController::class, 'tambah'])->name('/user/tambah');
-Route::put('/user/tambah_simpan',[UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
-Route::get('/user/ubah/{id}',[UserController::class, 'ubah'])->name('/user/ubah');
-Route::put('/user/ubah_simpan/{id}',[UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
-Route::get('/user/hapus/{id}',[UserController::class, 'hapus'])->name('/user/hapus');
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
+Route::get('/', [WelcomeController::class, 'index']);
