42cbb3c237d4fae692c43a7736caf6847fae905c
Rizxqi <rizqidewanto23@gmail.com>
2025-03-16 22:28:34 +0700

    Practicum 4 Jobsheet 5 (Layouting)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,80 +2,175 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Monolog\Level;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::with('level')->get();
-        return view('user',['data'=>$user]);
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
+        $breadcrumb = (object) [
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
+
+        $page = (object)[
+            'title' => 'Daftar user yang telah terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'user';
+
+        $level = LevelModel::all();
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'level'=> $level]);
     }
 
-    public function tambah()
+    public function list(Request $request)
     {
-        return view('user_tambah');
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
+            ->with('level');
+
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
+        return DataTables::of($users)
+            // Tambahkan index untuk nomor urut
+            ->addIndexColumn()
+
+            // Tambahkan kolom aksi (Detail, Edit, Hapus)
+            ->addColumn('action', function ($user) {
+                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '" style="display: inline;">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Are you sure you want to delete this data?\');">Delete</button></form>';
+                return $btn;
+            })
+
+            // Memberitahu bahwa kolom action berisi HTML
+            ->rawColumns(['action'])
+
+            ->make(true);
     }
 
-    public function tambah_simpan(Request $request)
+    public function create()
     {
+        $breadcrumb = (object) [
+            'title' => 'Tambah user',
+            'list' => ['Home', 'User', 'Tambah']
+        ];
+
+        $page = (object) [
+            'title' => 'Tambah user baru'
+        ];
+
+        $level = LevelModel::all(); // ambil data level untuk ditampilkan di form
+        $activeMenu = 'user'; // set menu yang sedang aktif 
+
+        return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+    }
+
+    public function show(string $id)
+    {
+        $user = UserModel::with('level')->find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Detail User',
+            'list' =>['Home', 'User', 'Detail']
+        ];
+
+        $page = (object) [
+            'title' => 'Detail User'
+        ];
+
+        $activeMenu = 'user';
+
+        $level = LevelModel::all();
+        return view('user.show',['breadcrumb'=>$breadcrumb,'page'=> $page,'user'=>$user,'activeMenu'=>$activeMenu]);
+    }
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100',
+            'password' => 'required|min:5',
+            'level_id' => 'required|integer'
+        ]);
+
         UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
-            'password' => Hash::make($request->password),
+            'password' => bcrypt($request->password),
             'level_id' => $request->level_id
         ]);
-        return redirect('/user');
+
+        return redirect('/user')->with('success', 'Data berhasil disimpan');
+    }
+    
+    /**
+     * edit.
+     */
+    public function edit(string $id)
+    {
+        $user = UserModel::find($id);
+        $level = LevelModel::all();
+        $breadcrumb = [
+            'title' => 'Edit User',
+            'list' => ['Home', 'User', 'Edit']
+        ];
+
+        $page = (object)[
+            'title' => 'Edit User'
+        ];
+
+        $activeMenu = 'user';
+
+        return view('user.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
-    public function ubah($id)
+    /**
+     * Update.
+     */
+    public function update(Request $request, string $id)
     {
-        // $user = UserModel::find($id);
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_user,username,' . $id . ',user_id',
+            'nama' => 'required|string|max:100',
+            'password' => 'nullable|min:5',
+            'level_id' => 'required|integer',
+        ]);
 
-        // // if (!$user) {
-        // //     return redirect('/user')->with('error', 'User tidak ditemukan');
-        // // }
-        // // $user = User::find($id);
-        // return view('user_ubah', ['data' => $user]);
+        UserModel::find($id)->update([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => $request->password ? bcrypt($request->password) : UserModel::find($id)->password,
+            'level_id' => $request->level_id,
+        ]);
 
-        // Fetch the user by ID
-        $user = UserModel::find($id);
+        return redirect('/user')->with('success', 'Data user berhasil dirubah');
+    }
 
-        // Check if the user exists
-        if (!$user) {
-            return redirect()->route('/user')->with('error', 'User not found');
+    public function destroy(string $id)
+    {
+        $check = UserModel::find($id);
+        if (!$check) {
+            return redirect('/user')->with('error', 'Data user tidak ditemukan');
         }
 
-        // Pass the user data to the view
-        return view('user_ubah', ['data' => $user]);
+        try {
+            UserModel::destroy($id);
 
-    }
+            return redirect('/user')->with('success', 'Data user berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
 
-    public function ubah_simpan($id, Request $request)
-    {
-        $user = UserModel::find($id);
-    
-        // if (!$user) {
-        //     return redirect('/user')->with('error', 'User tidak ditemukan');
-        // }
-    
-        $user->username = $request->username;
-        $user->nama = $request->nama;
-        $user->level_id = $request->level_id;
-    
-        $user->save();
-        return redirect('/user')->with('success', 'User berhasil diperbarui');
-    }
-
-    public function hapus($id)
-    {
-        $user = UserModel::find($id);
-        $user->delete();
-
-        return redirect('/user');
+            return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.template')
+
 @section('content')
     <div class="card card-outline card-primary">
         <div class="card-header">
@@ -9,71 +10,98 @@
         </div>
         <div class="card-body">
             @if (session('success'))
-                <div class="alert alert-success">{{ session('success') }}</div>
+                <div class="alert alert-success alert-dismissible">
+                    <h5><i class="icon fas fa-check"></i> Berhasil</h5>
+                    {{ session('success') }}
+                </div>
             @endif
             @if (session('error'))
-                <div class="alert alert-danger">{{ session('error') }}</div>
+                <div class="alert alert-danger alert-dismissible">
+                    <h5><i class="icon fas fa-ban"></i> Kesalahan</h5>
+                    {{ session('error') }}
+                </div>
             @endif
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Name</th>
-                        <th>User Level</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-    </div>
-@endsection
-@push('css')
-@endpush
-@push('js')
-    <script>
-        $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
-                processing: true, // Menampilkan indikator loading saat data diambil
-                serverSide: true, // Menggunakan server-side processing
-                ajax: {
-                    url: "{{ url('user/list') }}", // URL untuk mengambil data
-                    type: "POST",
-                    dataType: "json"
-                },
-                columns: [{
-                        data: "DT_RowIndex",
-                        className: "text-center",
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: "username",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "nama",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "level.level_nama", // Mengambil data level dari relasi ORM
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: "action",
-                        orderable: false,
-                        searchable: false
-                    }
-                ],
-                columnDefs: [{
-                        targets: [0, 4],
-                        className: "text-center"
-                    } // Memusatkan teks pada kolom No & Aksi
-                ]
-            });
-        });
-    </script>
-@endpush
+            <div class="row">
+                <div class="col-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Level</label>
+                        <div class="col-3">
+                            <select class="form-control" id="level_id" name="level_id" required>
+                                <option value="">- Semua -</option>
+                                @foreach ($level as $item)
+                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
+                    <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Nama</th>
+                                <th>Level Pengguna</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        @endsection
+
+        @push('css')
+        @endpush
+
+        @push('js')
+            <script>
+                $(document).ready(function() {
+                    var dataUser = $('#table_user').DataTable({
+                        serverSide: true,
+                        ajax: {
+                            url: "{{ url('user/list') }}",
+                            dataType: "json",
+                            type: "POST",
+                            data: function(d) {
+                                d.level_id = $('#level_id').val();
+                            }
+                        },
+                        columns: [{
+                                data: "DT_RowIndex",
+                                className: "text-center",
+                                orderable: false,
+                                searchable: false
+                            },
+                            {
+                                data: "username",
+                                className: "",
+                                orderable: true,
+                                searchable: true
+                            },
+                            {
+                                data: "nama",
+                                className: "",
+                                orderable: true,
+                                searchable: true
+                            },
+                            {
+                                data: "level.level_nama",
+                                className: "",
+                                orderable: false,
+                                searchable: false
+                            },
+                            {
+                                data: "action",
+                                className: "",
+                                orderable: false,
+                                searchable: false
+                            }
+                        ]
+                    });
+
+                    $('#level_id').change(function() {
+                        dataUser.ajax.reload();
+                    });
+                });
+            </script>
+        @endpush
