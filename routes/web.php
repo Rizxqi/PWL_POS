560a940609bfe47ed9766f8104d91d816d39ebc1
Rizxqi <rizqidewanto23@gmail.com>
2025-04-15 16:58:54 +0700

    Practicum 3 Jobsheet 6

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}',[UserController::class, 'show']);       // menampilkan detail user
     Route::get('/{id}/edit',[UserController::class, 'edit']);  // menampilkan halaman form edit user
     Route::put('/{id}',[UserController::class, 'update']);     // menyimpan perubahan data user
-    Route::get('/{id}/edit_ajax',[UserController::class, 'edit_ajax']);  // menghapus
-    Route::put('/{id}/update_ajax',[UserController::class, 'update_ajax']);  // menghapus
-    Route::delete('/{id}',[UserController::class, 'destroy']); // menghapus data user 
+    Route::get('/{id}/edit_ajax',[UserController::class, 'edit_ajax']);  
+    Route::put('/{id}/update_ajax',[UserController::class, 'update_ajax']);  
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); 
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); 
+    Route::delete('/{id}',[UserController::class, 'destroy']); 
 });     
+
